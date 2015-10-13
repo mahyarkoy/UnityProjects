b77b10c66c87c8823269aeefd59a47aecceab716
Jeff Kwok <jkwok9797@gmail.com>
2015-10-13 18:52:30 -0400

    Added PART1 Navigation

---
+++ BAssignments/B1/NavTest/Assets/Scripts/AgentController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AgentController : MonoBehaviour {
+
+	private Rigidbody rb;
+	private Renderer rend;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+		rend = GetComponent<Renderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(this.CompareTag("select"))
+			rend.material.color = Color.green;
+		else
+			rend.material.color = Color.red;
+	}
+}
---
+++ BAssignments/B1/NavTest/Assets/Scripts/NewObstacleController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewObstacleController : MonoBehaviour {
+	
+	Rigidbody rb;
+	Renderer rend;
+	
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		rend = GetComponent<Renderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(this.CompareTag("select")) {
+			rend.material.color = Color.yellow;
+			Vector3 move = new Vector3(0,0,0);
+			if(Input.GetButton("left")) {
+				move.x = move.x - .1f;
+			}
+			if(Input.GetButton("right")) {
+				move.x = move.x + .1f;
+			}
+			if(Input.GetButton("up")) {
+				move.z = move.z + .1f;
+			}
+			if(Input.GetButton("down")) {
+				move.z = move.z - .1f;
+			}
+			rb.position = rb.position + move;
+		}
+		else
+			rend.material.color = Color.blue;
+	}
+}
