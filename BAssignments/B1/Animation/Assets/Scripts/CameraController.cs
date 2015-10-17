84265ac8423f2d17c8a8fb4a8cfa19f4b1ece414
Jeff Ames <jeff.ames@cs.rutgers.edu>
2015-10-17 12:54:32 -0400

    Add animation project

---
+++ BAssignments/B1/Animation/Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+    public float distanceAway;
+    public float distanceUp;
+
+    public Transform target;
+
+    void Start()
+    {
+    }
+
+    void LateUpdate()
+    {
+        transform.position = target.position + Vector3.up * distanceUp - target.forward * distanceAway;
+        transform.LookAt(target);
+        transform.Rotate(new Vector3(-20, 0, 0));
+    }
+}
---
+++ BAssignments/B1/Animation/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public float jumpSpeed;
+
+    private Animator animator;
+
+	void Start()
+    {
+        animator = GetComponent<Animator>();
+	}
+
+	void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        float jump = Input.GetAxis("Jump");
+        float run = Input.GetAxis("Run");
+
+        float effectiveSpeed = speed;
+        if (run > 0)
+            effectiveSpeed *= 2;
+
+        animator.SetFloat("Speed", v * effectiveSpeed);
+        animator.SetBool("Jump", jump > 0);
+
+        transform.Rotate(new Vector3(0, h * effectiveSpeed, 0) * Time.deltaTime);
+	}
+}
