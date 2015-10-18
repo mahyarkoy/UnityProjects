7f1d0422bb674939a98e2ee631ed094a8a5cbccd
James Lee <jyl50@scarletmail.rutgers.edu>
2015-10-17 22:45:18 -0400

    Part1

---
+++ BAssignments/B1/AssignmentB1/Assets/Script/playercontroller.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playercontroller : MonoBehaviour {
+
+	public float jump;
+	
+
+	void FixedUpdate ()
+	{
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			this.GetComponent<Rigidbody>().AddForce (new Vector3 (0, jump, 0));
+		}
+	}
+
+}
