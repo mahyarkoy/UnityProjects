9d01bac4bb9ebed9723b58c9764c5a82d764118f
RoVernekar <ro.vernekar@gmail.com>
2015-10-17 22:36:27 -0400

    Finished part 2

---
+++ BAssignments/B1/B1/Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    Animator animator;
+
+	// Use this for initialization
+	void Start () {
+        animator = GetComponentInChildren<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        bool sprint = Input.GetButton("Fire3");
+        bool jump = Input.GetButtonDown("Jump");
+
+        animator.SetFloat("Forward", v);
+        animator.SetFloat("Turn", h);
+        animator.SetBool("Sprint", sprint);
+        animator.SetBool("Jump", jump);
+    }
+}
