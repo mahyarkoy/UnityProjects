7c9e0cba5aa835cc7e3e88dd97b484cfc762be70
Diana Kim <dsk101@rutgers.edu>
2015-12-17 17:14:30 -0500

    add characters and meeting on e point

--- BAssignments/B3/Assets/Scripts/BehaviorTree.cs
+++ BAssignments/B3/Assets/Scripts/BehaviorTree.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 using TreeSharpPlus;
 
 public enum StoryStatus //story state according to the input status.
@@ -27,15 +28,17 @@ public enum InputStatus
 
 public class BehaviorTree : MonoBehaviour
 {
-    public Transform meetingPointChar1;
-    public Transform meetingPointChar2;
-    public Transform meetingPointChar3;
-    public Transform meetingPointChar4;
+	public GameObject  meetingPointChar1;
+	//GameObject meetingPointChar2;
+    //public Transform meetingPointChar3;
+    //public Transform meetingPointChar4;
 
-    public GameObject Char1;
-    public GameObject Char2;
-    public GameObject Char3;
-    public GameObject Char4;
+    public GameObject Chris;
+    public GameObject Daniel;
+	public GameObject Tom;
+	public GameObject Harry;
+   //public GameObject Char3;
+    //public GameObject Char4;
 
     private BehaviorAgent behaviorAgent;
 
@@ -121,7 +124,7 @@ public class BehaviorTree : MonoBehaviour
         Val<Vector3> position = Val.V (() => target.position);
         return new Sequence(participants.GetComponent<BehaviorMecanim>().Node_GoTo(position), new LeafWait(1000));
     }
-
+	/*
     protected Node ST_Appraoch_at_certain_raidusAndWait(GameObject participants, Transform target, float radius)
     {
         Val<Vector3> position = Val.V (() => target.position);
@@ -145,10 +148,9 @@ public class BehaviorTree : MonoBehaviour
     protected Node ST_story_one()
     {
         Node walkToMeetingPoint = new SequenceParallel(
-            ST_ApproachAndWait(Char1, meetingPointChar1),
-            ST_ApproachAndWait(Char2, meetingPointChar2),
-            ST_ApproachAndWait(Char3, meetingPointChar3),
-            ST_ApproachAndWait(Char4, meetingPointChar4));
+            ST_ApproachAndWait(Chris, meetingPointChar1),
+			ST_ApproachAndWait(Daniel, meetingPointChar2),
+            );
 
         if (walkToMeetingPoint.LastStatus == RunStatus.Running)
             meet_one_point = true;
@@ -165,10 +167,42 @@ public class BehaviorTree : MonoBehaviour
             ST_ApproachAndWait(Char4, meetingPointChar4));
 
         return new SelectorParallel(new Sequence(walkToMeetingPoint), new LeafWait(10000));
+    }*/
+
+	protected Node ST_ApproachAndWait(GameObject player, GameObject meet_position)
+    {
+        Val<Vector3> position = Val.V (() => meet_position.transform.position);
+        return new Sequence( player.GetComponent<BehaviorMecanim>().Node_GoTo(position), new LeafWait(1000));
+    }
+    protected Node ST_Meet_Wait(GameObject player, GameObject meet_position,int dist)
+    {
+        Val<Vector3> meet_point = Val.V (() => meet_position.transform.position);
+        Node meet_to_one = new Sequence(player.GetComponent<BehaviorMecanim>().Node_GoToUpToRadius(meet_point,dist), new LeafWait(1000));
+        return meet_to_one;
     }
 
-    protected Node BuildTreeRoot()
-    {
-        return new DecoratorLoop(new Sequence(ST_story_one(), ST_story_two()));
-    }
+    
+	protected Node BuildTreeRoot() 
+	{
+
+		Func<bool> story1  = () => true; // we have not implemented state of story.
+	
+		Node train_play  = new DecoratorLoop (
+			/*new Sequence(
+                this.ST_ApproachAndWait(this.Chris,this.meeting_point),
+                this.ST_ApproachAndWait(this.Daniel,this.meeting_point),
+                this.ST_ApproachAndWait(this.Tom,this.meeting_point),
+                this.ST_ApproachAndWait(this.Harry,this.meeting_point)));*/
+
+			new SequenceParallel(this.ST_Meet_Wait(this.Tom,this.meetingPointChar1,2),this.ST_Meet_Wait(this.Chris,this.meetingPointChar1,2),this.ST_Meet_Wait(this.Harry,this.meetingPointChar1,2),this.ST_Meet_Wait(this.Daniel,this.meetingPointChar1,2)));
+
+		Node trigger = new DecoratorLoop (new LeafAssert (story1));
+		Node root_story = new DecoratorLoop (new DecoratorForceStatus (RunStatus.Success, new SequenceParallel(trigger,train_play)));
+
+		return root_story;
+
+	}
 }
+
+
+
