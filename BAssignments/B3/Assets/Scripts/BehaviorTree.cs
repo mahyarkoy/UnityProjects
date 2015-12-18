6a1f839c7c9f6fe954b625ab3cb7d4da21e510d6
Jeff Ames <jeff.ames@cs.rutgers.edu>
2015-12-18 13:51:17 -0500

    Try making others follow Tom as he walks

--- BAssignments/B3/Assets/Scripts/BehaviorTree.cs
+++ BAssignments/B3/Assets/Scripts/BehaviorTree.cs
@@ -246,16 +246,11 @@ public class BehaviorTree : MonoBehaviour
         Val<Vector3> point5 = Val.V (() => trainPoint5.transform.position);
 
 		return new Sequence(
-            Tom.GetComponent<BehaviorMecanim>().Node_GoTo(point1),
-            ST_others_follow(Tom),
-            Tom.GetComponent<BehaviorMecanim>().Node_GoTo(point2),
-            ST_others_follow(Tom),
-            Tom.GetComponent<BehaviorMecanim>().Node_GoTo(point3),
-            ST_others_follow(Tom),
-            Tom.GetComponent<BehaviorMecanim>().Node_GoTo(point4),
-            ST_others_follow(Tom),
-            Tom.GetComponent<BehaviorMecanim>().Node_GoTo(point5),
-            ST_others_follow(Tom));
+            new SequenceParallel(Tom.GetComponent<BehaviorMecanim>().Node_GoTo(point1), ST_others_follow(Tom)),
+            new SequenceParallel(Tom.GetComponent<BehaviorMecanim>().Node_GoTo(point2), ST_others_follow(Tom)),
+            new SequenceParallel(Tom.GetComponent<BehaviorMecanim>().Node_GoTo(point3), ST_others_follow(Tom)),
+            new SequenceParallel(Tom.GetComponent<BehaviorMecanim>().Node_GoTo(point4), ST_others_follow(Tom)),
+            new SequenceParallel(Tom.GetComponent<BehaviorMecanim>().Node_GoTo(point5), ST_others_follow(Tom))));
     }
 
 	protected Node BuildTreeRoot()
