cc22345207febacda2b7408b4ad7a9fd16e88889
Jeff Ames <jeff.ames@cs.rutgers.edu>
2015-12-18 21:45:16 -0500

    Add mouse UI for selecting agents

---
+++ BAssignments/B3/Assets/Scripts/MouseUI.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseUI : MonoBehaviour
+{
+    public Camera camera;
+
+	void Start()
+    {
+    }
+
+	void Update()
+    {
+        RaycastHit hit;
+
+        if (Input.GetMouseButtonDown(0)) {
+            if (Physics.Raycast(camera.ScreenPointToRay(Input.mousePosition), out hit, 100)) {
+                switch (hit.transform.gameObject.tag) {
+                    case "Daniel":
+                        print("Daniel selected");
+                        break;
+                    case "Chris":
+                        print("Chris selected");
+                        break;
+                    case "Harry":
+                        print("Harry selected");
+                        break;
+                    case "Tom":
+                        print("Tom selected");
+                        break;
+                }
+            }
+        }
+	}
+}
